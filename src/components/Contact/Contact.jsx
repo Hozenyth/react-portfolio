e7b9eff1fc36abf612c9949e81587a9300261519
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-11-12 10:13:00 -0300

    atualização contato

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,24 +7,24 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contato</h2>
+        <p>Sinta-se à vontade para entrar em contato!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:hozenyth.andrade@gmail.com">hozenyth.andrade@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/hozenyth/">linkedin.com/Hozenyth</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com/Hozenyth">github.com/Hozenyth</a>
         </li>
       </ul>
     </footer>
