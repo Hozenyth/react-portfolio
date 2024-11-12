5aa9b98bc4928ef7de94125d9ddfc5086f851820
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-11-12 10:46:11 -0300

    atualização contato.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,19 +12,19 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:hozenyth.andrade@gmail.com">hozenyth.andrade@gmail.com</a>
+          <img src={getImageUrl("contact/whats.png")} alt="WhatsApp icon" />
+          <a className={styles.contatoDestaque} href="https://api.whatsapp.com/send?phone=5522999326308&text=Olá%2C%20gostaria%20de%20mais%20informações!" target="_blank"  rel="noopener noreferrer">Entre em contato pelo WhatsApp!</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/hozenyth/">linkedin.com/Hozenyth</a>
+          <a href="https://www.linkedin.com/in/hozenyth/" target="_blank"  rel="noopener noreferrer">linkedin.com/Hozenyth</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/Hozenyth">github.com/Hozenyth</a>
+          <a href="https://www.github.com/Hozenyth" target="_blank"  rel="noopener noreferrer">github.com/Hozenyth</a>
         </li>
       </ul>
     </footer>
