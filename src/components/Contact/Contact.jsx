f4d0d47150324154a02985c44f8c7e479feed67f
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-11-13 16:07:08 -0300

    atualização contato

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,13 +8,15 @@ export const Contact = () => {
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contato</h2>
+        <ul>
         <p>Sinta-se à vontade para entrar em contato!</p>
-      </div>
-      <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/whats.png")} alt="WhatsApp icon" />
           <a className={styles.contatoDestaque} href="https://api.whatsapp.com/send?phone=5522999326308&text=Olá%2C%20gostaria%20de%20mais%20informações!" target="_blank"  rel="noopener noreferrer">Entre em contato pelo WhatsApp!</a>
         </li>
+        </ul>       
+      </div>
+      <ul className={styles.links}>      
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
